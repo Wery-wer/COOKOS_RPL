011545346b0e1311646d18b3a65e0dc7bd411cb9
Bodan07 <najmi.kamil@gmail.com>
2023-10-14 16:06:13 +0700

    Add Melihat_Profile

---
+++ lib/screen/Melihat_profile.dart
@@ -0,0 +1,117 @@
+//import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class seeProfile extends StatefulWidget {
+  const seeProfile({Key? key}) : super(key: key);
+
+  @override
+  State<seeProfile> createState() => _seeProfile();
+}
+
+class _seeProfile extends State<seeProfile> {
+  TextEditingController editController = TextEditingController();
+  TextEditingController bookmarkController = TextEditingController();
+
+  void _edit() {
+    String edit = editController.text;
+  }
+
+  void _bookmark() {
+    String bookmark = bookmarkController.text;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
+          ),
+          backgroundColor: Color(0xffe5737d),
+          centerTitle: true,
+          title: Text(
+            'My Profile',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Rubik',
+                fontSize: 20,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 250,
+                width: double.infinity,
+                color: const Color(0xffe5737d),
+                child: (Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50),
+                      width: 150,
+                      height: 150,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                )),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 40),
+                child: const Text(
+                  'Rinrin Marinka',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w700,
+                    fontSize: 15,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: _edit,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: const Color(0xffe5737d), // Ubah warna teks
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    minimumSize: const Size(230, 50),
+                  ),
+                  child: const Text(
+                    'Edit',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: _bookmark,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Color.fromARGB(255, 255, 255, 255),
+                    backgroundColor: const Color(0xffe5737d), // Ubah warna teks
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    minimumSize: const Size(230, 50),
+                  ),
+                  child: const Text(
+                    'Bookmark',
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
