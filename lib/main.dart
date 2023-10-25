9735ee07ba14745023f1931bbc81589069932892
Wery <44234831+Wery-wer@users.noreply.github.com>
2023-10-25 19:59:56 +0700

    update login dan main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dev/firebase_options.dart';
 import 'package:flutter_dev/screen/edit_profile.dart';
 import 'package:flutter_dev/screen/logout.dart';
 import 'package:flutter_dev/screen/meilhat_resep.dart';
@@ -13,45 +11,41 @@ import 'package:flutter/services.dart';
 import 'package:flutter_dev/screen/registrasi.dart';
 import 'package:flutter_dev/screen/test.dart';
 import 'package:get/get.dart';
-
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        // get material app
+  
+   // This widget is the root of your application.
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(   // get material app
         debugShowCheckedModeBanner: false,
         title: 'COOKOS',
         theme: ThemeData(fontFamily: 'Poppins'),
         //initialRoute: LoginPage.routeName,
-        //home: LoginPage(),
-        routes: {
-          '/': (context) => const SplashScreenPage(),
-          '/login': (context) => const LoginPage(),
-          '/registrasi': (context) => const Registrasi(),
-          '/edit_profile': (context) => const EditProfile(),
-          '/Melihat_profile': (context) => const seeProfile(),
-          '/filter_resep': (context) => const filterResep(),
-          '/rating_review': (context) => const RatingPage(),
+        home : RatingPage(),
+        routes:   {
+        // '/': (context) => const SplashScreenPage(),
+        // '/login': (context) => const LoginPage(),
+        //   '/registrasi': (context) => const Registrasi(),
+        // '/edit_profile': (context) => const EditProfile(),
+          // '/Melihat_profile': (context) => const seeProfile(),
+          // '/filter_resep' :(context) => const filterResep(),
+          
+          '/rating_review' :(context) => const RatingPage(),
+          
+
         }
         // getPages: [
         //   GetPage(name: LoginPage.routeName, page: () => const LoginPage()),
         //   GetPage(name: Registrasi.routeName, page: () => const Registrasi()),
         // ],
-        );
-  }
-}
+     ); 
+   } 
+ }
 
 //  void main() {
 //   runApp(const MaterialApp(
